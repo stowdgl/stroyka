b3f8d668e166d3bbd1780b9fbacd09adca032874
StowdGl <gleb.khivrenko@gmail.com>
2018-11-22 21:42:45 +0200

    Added class Material

---
+++ subject.php
@@ -0,0 +1,12 @@
+<?php
+
+class Material extends Subject{
+    public $valueC = array('Metres','Tonns', 'Apiece','Boxes', 'Bags','Kilos','Litres'), $valueR=0;
+    function __construct($valueC)
+    {
+        $this->valueR = $this->valueC[$valueC];
+    }
+    function info(){
+        echo $this->valueR;
+    }
+}
