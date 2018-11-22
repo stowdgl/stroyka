0d5d5c85d8f0f882fa4016744d63a5d0049cc0ee
Bandydan <bandydan@gmail.com>
2018-11-22 20:05:24 +0200

    beginning!

---
+++ classes.php
@@ -0,0 +1,39 @@
+User:
+
+	Director
+
+	Stockman
+
+	Supplier
+
+	TeamLead
+
+	Worker (types)
+
+	Driver
+
+Order
+
+Orderline
+
+Transaction
+
+Transactionline
+
+
+
+
+
+Object
+
+Storage
+
+Subject:
+
+Type
+
+Instrument
+
+Material
+
+Mechanizm
