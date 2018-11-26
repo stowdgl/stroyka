d3ea09c17525a8b891b1f04719c7f0c9283ea746
Vlasssta <Vla23sta@gmail.com>
2018-11-26 20:25:01 +0300

    by Vlasta

--- classes.php
+++ classes.php
@@ -1,8 +1,39 @@
-User:
+<?php
 
-	Director
+abstract class User {
 
-	Stockman
+    public $fullname;
+
+    public $accessRights;
+
+    abstract public function getFullname();
+
+    abstract public function getAccessRights();
+
+}
+class TeamLead extends  User {
+    public $accessRights;
+    public  $name;
+    public  $surname;
+    public function __construct($name,$surname,$accessRights){
+       $this ->name=$name;
+       $this->surame=$surname;
+       $this->accessRights=$accessRights;
+    }
+    }
+
+    function getFullname(){
+        return  $this->name.''.$this->surname;
+    }
+    function getAccessRights(){
+        return  $this->accessRights;
+    }
+    function make_order(){
+        return $this->orderBlank;
+    }
+    function workPlan(){
+         return this->
+//	Stockman
 
 	Supplier
 
